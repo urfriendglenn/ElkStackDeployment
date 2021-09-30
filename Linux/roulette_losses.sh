2a99b5bdc351198cb0962bfc5f70de9fa752a54c
Glenn Lewis <33062460+urfriendglenn@users.noreply.github.com>
2021-09-29 19:34:34 -0500

    Delete Linux directory

--- Linux/roulette_losses.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Takes three arguments: Time (12:00:00), AM/PM, File Name
-
-echo -n "Date: " && echo $3 | awk -F "_" '{print$1}'
-grep "$1 $2" $3 | awk -F" " '{print$1,$2,$5,$6}'
-echo "-------"
